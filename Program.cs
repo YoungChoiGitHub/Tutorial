a8426791dd1f94120340ead7a94c8c20b4d32126
YoungChoi <young.il.choi@hotmail.com>
2015-08-22 09:01:06 -0400

    assignment 3

--- Program.cs
+++ Program.cs
@@ -4,21 +4,212 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace Assignment2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HelloWorld");
-	    Console.WriteLine("I am happy");
-	/* add comment */
-	/* add comment */
-	Console.Readline(); // for debugging
-	// add comment
-	// changes made from clone directory
+            Customer[] customerArray = new Customer[30];
+            int arrayIndex = 0;
 
-	// add comment from master
+            // Automobile customer
+            Customer newCust = new Customer();
+            newCust.FirstName = "Joe";
+            newCust.LastName = "Smith";
+            Order newOrder = new Order();
+            newOrder.PlaceOrder(ModelName.BMW520);
+            newCust.SaveOrder(newOrder);
+            customerArray[arrayIndex++] = newCust;
+
+            newCust = new Customer();
+            newCust.FirstName = "Tom";
+            newCust.LastName = "Cruise";
+            newOrder = new Order();
+            newOrder.PlaceOrder(ModelName.BMW235, 28500, 1); // special offer
+            newCust.SaveOrder(newOrder);
+            customerArray[arrayIndex++] = newCust;
+
+
+
+            // Motocycle customer
+            newCust = new Customer();
+            newCust.FirstName = "Sally";
+            newCust.LastName = "Jones";
+            newOrder = new Order();
+            newOrder.PlaceOrder(ModelName.HondaCruiser);
+            newCust.SaveOrder(newOrder);
+            customerArray[arrayIndex++] = newCust;
+
+            newCust = new Customer();
+            newCust.FirstName = "Rick";
+            newCust.LastName = "White";
+            newOrder = new Order();
+            newOrder.PlaceOrder(ModelName.HondaSport, 17500.0, 2); // special offer
+            newCust.SaveOrder(newOrder);
+            customerArray[arrayIndex++] = newCust;
+
+
+            // Display Result
+            foreach (var c in customerArray)
+            {
+                if (c == null) break;
+                Console.WriteLine(c.FirstName + ' ' + c.LastName);
+
+                foreach (var o in c.Orders)
+                {
+                    if (o == null) break;
+                    Console.WriteLine("{0} - {1:C} - {2, 0:N1} (square feet)", 
+                        o.OrderedVehicle.Description,
+                        o.Price,
+                        o.OrderedVehicle.CargoCapacity());
+                    Console.WriteLine("Quantity: {0} - Model Name: {1} - Horse Power: {2}",
+                        o.Quantity,
+                        o.Model, 
+                        o.OrderedVehicle.HorsePower);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();  // for checking result
         }
     }
+
+    public enum ModelName
+    {
+        BMW520, BMW320, BMW235, // for Automobile
+        HondaTouring, HondaCruiser, HondaSport // for Motocycle
+    }
+    public class Customer 
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Order[] Orders = new Order[5];
+        private int numOrders = 0;
+
+        public void SaveOrder(Order order) 
+        {
+            Orders[numOrders++] = order;
+        }
+    }
+
+    public class Order
+    {
+        public string Model { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public Vehicle OrderedVehicle { get; set; }
+
+        public void PlaceOrder(ModelName modelName)
+        {
+            switch (modelName)
+            {
+                case ModelName.BMW520:
+                    this.PlaceOrder(modelName, 50000.0, 1);
+                    break;
+                case ModelName.BMW320:
+                    this.PlaceOrder(modelName, 45000.0, 1);
+                    break;
+                case ModelName.BMW235:
+                    this.PlaceOrder(modelName, 30000.0, 1);
+                    break;
+                case ModelName.HondaCruiser:
+                    this.PlaceOrder(modelName, 25000.0, 1);
+                    break;
+                case ModelName.HondaSport:
+                    this.PlaceOrder(modelName, 20000.0, 1);
+                    break;
+                case ModelName.HondaTouring:
+                    this.PlaceOrder(modelName, 15000.0, 1);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void PlaceOrder(ModelName modelName, double price, int qualtity)
+        { 
+            Model = modelName.ToString();
+            Price = price;
+            Quantity = qualtity;
+
+            switch (modelName)
+            {
+                case ModelName.BMW520:
+                    OrderedVehicle = new Automobile(250, "Automobile", 4.0, 6.0, 2.5);
+                    break;
+                case ModelName.BMW320:
+                    OrderedVehicle = new Automobile(220, "Automobile", 3.5, 5.0, 2);
+                    break;
+                case ModelName.BMW235:
+                    OrderedVehicle = new Automobile();
+                    break;
+                case ModelName.HondaCruiser:
+                    OrderedVehicle = new Motocycle(70, "Motocycle", 1.5, 1.5);
+                    break;
+                case ModelName.HondaSport:
+                    OrderedVehicle = new Motocycle(60, "Motocycle", 1.2, 1.2);
+                    break;
+                case ModelName.HondaTouring:
+                    OrderedVehicle = new Motocycle();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public class Vehicle
+    { 
+        public int Wheels { get; set; }
+        public int HorsePower { get; set; }
+        public string Description { get; set; }
+        public double CargoLength { get; set; }
+        public double CargoWidth { get; set; }
+        public double CargoHeight { get; set; }
+
+        public virtual double CargoCapacity() 
+        {
+            return CargoLength * CargoWidth * CargoHeight;
+        }
+    }
+
+    public class Automobile : Vehicle
+    { 
+        public Automobile() : this(200, "Automobile", 3.0, 5.0, 2.0)
+        {
+        }
+        public Automobile(int horsePower, string description, double length, double width, double height)
+        {
+            Wheels = 4;
+            HorsePower = horsePower;
+            Description = description;
+            CargoLength = length;
+            CargoWidth = width;
+            CargoHeight = height;
+        }
+
+    }
+
+    public class Motocycle : Vehicle
+    {
+        public double CargoRadius { get; set; }
+
+                public Motocycle() : this(50, "Motocycle", 1.0, 1.0)
+        {
+        }
+                public Motocycle(int horsePower, string description, double radius, double height)
+        {
+            Wheels = 2;
+            HorsePower = horsePower;
+            Description = description;
+            CargoRadius = radius;
+            CargoHeight = height;
+        }
+
+        public override double CargoCapacity()
+        {
+ 	        return (Math.PI * CargoRadius * CargoRadius * CargoHeight) * 2; // Both sides
+        }
+    }
+
 }
